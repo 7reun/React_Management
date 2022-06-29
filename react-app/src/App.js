eb5b7efe0cd7aa9d5b2a4f606cbe6a257251def4
lge <lgevs@naver.com>
2022-06-29 13:37:35 +0900

    modify App.js state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,9 @@
 //import logo from './logo.svg';
 import './App.css';
 import React,{Component} from 'react';
-//import TOC from './components/TOC';
-//import CONTENTS from './components/contents';
-//import Subject from './components/subject';
+import TOC from './components/TOC';
+import CONTENTS from './components/contents';
+import Subject from './components/subject';
 
 
 //컴포넌트는 분리해서 따로 만들어줌.
@@ -53,36 +53,26 @@ import React,{Component} from 'react';
 
 //function App() {
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:'WEB',sub:'world wild web'},
+      contents:[
+        {id:1,title:'HTML',desc:'HTML is for information'},
+        {id:2,title:'CSS',desc:'CSS is for design'},
+        {id:3,title:'JavaScript',desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
-      //<div>
-        //<Subject title="WEB" sub="world wide web"></Subject> {/* == <Subject/> */}
-        //<Subject title="REACT" sub="react"></Subject> 
-        //<TOC></TOC>
-        //<CONTENTS title="HTML" desc="HTML is HyperText MarkUp Language."></CONTENTS>
-      //</div>
-      //<div>hello world</div> 상위 태그가 하나인 구조로 만들어야함.
+      <div>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> {/* == <Subject/> */}
+        <TOC data={this.state.contents}></TOC>
+        <CONTENTS title="HTML" desc="HTML is HyperText MarkUp Language."></CONTENTS>
+      </div>
 
-      /*{ <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div> } */
-
-      ////////////////////////////////////////////
-
-      <div></div>
+      //<div>hello world</div> 상위 태그가 하나인 구조로 만들어야함.    
     );
   }
 }
